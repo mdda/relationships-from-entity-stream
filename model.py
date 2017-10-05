4c60a31b06250ec9179b204f761259ff85801a04
Martin Andrews <GitHub@mdda.net>
2017-10-06 01:42:41 +0800

    Pulling in Gumbel-softmax code

--- model.py
+++ model.py
@@ -18,6 +18,8 @@ class ConvInputModel(nn.Module):
         self.batchNorm3 = nn.BatchNorm2d(24)
         self.conv4 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
         self.batchNorm4 = nn.BatchNorm2d(24)
+        # This is now 24 channels in a 5x5 grid
+
 
         
     def forward(self, img):
@@ -196,3 +198,172 @@ class CNN_MLP(BasicModel):
         
         return self.fcout(x_)
 
+
+
+# https://www.reddit.com/r/MachineLearning/comments/6d44i7/d_how_to_use_gumbelsoftmax_for_policy_gradient/
+# The gumbel-softmax is for a more specific case that being able to approximate a gradient 
+# for any non-differentiable function. Softmax is exactly what is says on the tin; a soft-max. 
+# The max function is not differentiable but is often used to sample from a distribution 
+# by taking the highest probability. The softmax can be used to approximate the max function 
+# and is differentiable. So what you can do is take the max in the forward pass but use softmax 
+# during the backward pass in order to be able to pass gradients though it. 
+# You can then anneal the softmax function temperature so that the approximation gets closer and closer 
+# to the true max function during training to lower the error in the approximation.
+
+
+# Blog post : 
+#   http://blog.evjang.com/2016/11/tutorial-categorical-variational.html
+#   TF code : https://gist.github.com/ericjang/1001afd374c2c3b7752545ce6d9ed349
+
+# Keras notebook version : https://github.com/EderSantana/gumbel
+
+# Theano / Lasagne : https://github.com/yandexdataschool/gumbel_lstm
+#   For the 'hard' version, plain argmax is used (at https://github.com/yandexdataschool/gumbel_lstm/blob/master/gumbel_softmax.py#L81) 
+
+
+# afaik, unlike max, argmax (index of maximum) will have zero/NA gradient by definition 
+#  since infinitely small changes in the vector won't change index of the maximum unless there are two exactly equal elements.
+
+
+# From : https://github.com/pytorch/pytorch/issues/639
+#def gumbel_sampler(input, tau, temperature):
+#    noise = torch.rand(input.size())
+#    noise.add_(1e-9).log_().neg_()
+#    noise.add_(1e-9).log_().neg_()
+#    noise = Variable(noise)
+#    x = (input + noise) / tau + temperature
+#    x = F.softmax(x.view(input.size(0), -1))
+#    return x.view_as(input)
+
+# From : https://discuss.pytorch.org/t/stop-gradients-for-st-gumbel-softmax/530
+def sample_gumbel(input):
+    noise = torch.rand(input.size())
+    eps = 1e-20
+    noise.add_(eps).log_().neg_()
+    noise.add_(eps).log_().neg_()
+    return Variable(noise)
+
+def gumbel_softmax_sample(input, temperature=1.):
+    noise = sample_gumbel(input)
+    x = (input + noise) / temperature
+    x = F.log_softmax(x)
+    return x.view_as(input)
+
+
+
+class RFS(BasicModel):
+    def __init__(self, args):
+        super(RN, self).__init__(args, 'RFS')
+        
+        self.conv = ConvInputModel()
+        
+        # Let's create a X/Y coordinate thing (2d)
+        
+        # The 'keys' for the image will be the first 12 of the 24 /concat with/ the coordinate thing
+        # The 'values' for the image will be the second 12 of the 24 /concat with/ the coordinate thing
+        
+        # So the attention query vector (output) will need to be n_q=14 wide, and will retrieve a n_v=14 wide value
+        
+        # To get the attention weights, create a dot-product of the k&q numbers
+        # *  Option 1 : Softmax these.  
+        # *  Option 2 : use a temperature parameter on this vector Gumbel-SoftMax (Google ICLR-2017)
+        
+        # https://arxiv.org/abs/1611.01144
+        #   Categorical Reparameterization with Gumbel-Softmax
+        #     This distribution has the essential property that it can be smoothly annealed into a categorical distribution. 
+        #     Use this as an 'action' when temperature -> 0
+        # See also : 
+        #   https://casmls.github.io/general/2017/02/01/GumbelSoftmax.html
+        
+        # So, the input to the LSTM thing at the bottom will be n_v-wide
+        # Initial input should be (say) all-zero
+        
+        # Maximum length of objects to be processed is ~6.  
+        # So make LSTM stage 8 long
+        
+        
+        
+        ##(number of filters per object+coordinate of object)*2+question vector
+        self.g_fc1 = nn.Linear((24+2)*2+11, 256)
+
+        self.g_fc2 = nn.Linear(256, 256)
+        self.g_fc3 = nn.Linear(256, 256)
+        self.g_fc4 = nn.Linear(256, 256)
+
+        self.f_fc1 = nn.Linear(256, 256)
+
+        self.coord_oi = torch.FloatTensor(args.batch_size, 2)
+        self.coord_oj = torch.FloatTensor(args.batch_size, 2)
+        if args.cuda:
+            self.coord_oi = self.coord_oi.cuda()
+            self.coord_oj = self.coord_oj.cuda()
+        self.coord_oi = Variable(self.coord_oi)
+        self.coord_oj = Variable(self.coord_oj)
+
+        # prepare coord tensor
+        def cvt_coord(i):
+            return [(i/5-2)/2., (i%5-2)/2.]
+        
+        self.coord_tensor = torch.FloatTensor(args.batch_size, 25, 2)
+        if args.cuda:
+            self.coord_tensor = self.coord_tensor.cuda()
+        self.coord_tensor = Variable(self.coord_tensor)
+        np_coord_tensor = np.zeros((args.batch_size, 25, 2))
+        for i in range(25):
+            np_coord_tensor[:,i,:] = np.array( cvt_coord(i) )
+        self.coord_tensor.data.copy_(torch.from_numpy(np_coord_tensor))
+
+
+        self.fcout = FCOutputModel()
+        
+        self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
+
+
+    def forward(self, img, qst):
+        x = self.conv(img) ## x = (64 x 24 x 5 x 5) = (batch#, channels, x-s, y-s)
+        
+        """g"""
+        mb = x.size()[0]
+        n_channels = x.size()[1]
+        d = x.size()[2]
+        # x_flat = (64 x 25 x 24)
+        x_flat = x.view(mb,n_channels,d*d).permute(0,2,1)
+        
+        # add coordinates
+        x_flat = torch.cat([x_flat, self.coord_tensor],2)
+        
+        # add question everywhere
+        qst = torch.unsqueeze(qst, 1)
+        qst = qst.repeat(1,25,1)
+        qst = torch.unsqueeze(qst, 2)
+        
+        # cast all pairs against each other
+        x_i = torch.unsqueeze(x_flat,1) # (64x1x25x26+11)
+        x_i = x_i.repeat(1,25,1,1) # (64x25x25x26+11)
+        x_j = torch.unsqueeze(x_flat,2) # (64x25x1x26+11)
+        x_j = torch.cat([x_j,qst],3)
+        x_j = x_j.repeat(1,1,25,1) # (64x25x25x26+11)
+        
+        # concatenate all together
+        x_full = torch.cat([x_i,x_j],3) # (64x25x25x2*26+11)
+        
+        # reshape for passing through network
+        x_ = x_full.view(mb*d*d*d*d,63)
+        x_ = self.g_fc1(x_)
+        x_ = F.relu(x_)
+        x_ = self.g_fc2(x_)
+        x_ = F.relu(x_)
+        x_ = self.g_fc3(x_)
+        x_ = F.relu(x_)
+        x_ = self.g_fc4(x_)
+        x_ = F.relu(x_)
+        
+        # reshape again and sum
+        x_g = x_.view(mb,d*d*d*d,256)
+        x_g = x_g.sum(1).squeeze()
+        
+        """f"""
+        x_f = self.f_fc1(x_g)
+        x_f = F.relu(x_f)
+        
+        return self.fcout(x_f)
