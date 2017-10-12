1aeda2b1eac598e7e3d496c07a259cd2a9b7c81d
Martin Andrews <GitHub@mdda.net>
2017-10-12 23:57:55 +0800

    Try to run the network a little...

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 #python sort_of_clevr_generator.py
 
 #python main.py --model=RN      --epochs=20
+# Epoch 0 (end) :  Test set: Relation accuracy: 67% | Non-relation accuracy: 59%
 
 #python main.py --model=CNN_MLP --epochs=100
+# Epoch 0 (end) :  
 
+#python main.py --model=RFS     --epochs=20
+# Epoch 0 (end) :  
