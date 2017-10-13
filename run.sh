f35001a7a1ab0f3c5add6604dfcf501e2b726b18
Martin Andrews <GitHub@mdda.net>
2017-10-13 18:15:07 +0800

    Fuller CNN-MLP result summary (over-training)

--- run.sh
+++ run.sh
@@ -11,10 +11,19 @@
 
 
 #python -u main.py --model=CNN_MLP --epochs=100 | tee  model/training_CNN_MLP.log
-# Epoch 4 (end) :  Test set: Relation accuracy: 68% | Non-relation accuracy: 59%
+# Test set after epoch  1 : Relation accuracy: 53% | Non-relation accuracy: 59%
+# Test set after epoch  5 : Relation accuracy: 72% | Non-relation accuracy: 59%
+# Test set after epoch 10 : Relation accuracy: 73% | Non-relation accuracy: 62%
+# Test set after epoch 20 : Relation accuracy: 66% | Non-relation accuracy: 61%
+# Test set after epoch 40 : Relation accuracy: 68% | Non-relation accuracy: 61%
+# Test set after epoch 50 : Relation accuracy: 62% | Non-relation accuracy: 62%
+# Test set after epoch 70 : Relation accuracy: 69% | Non-relation accuracy: 66%
+# Test set after epoch 100 : Relation accuracy: 67% | Non-relation accuracy: 67%
+#  43secs per epoch on 760 GTX
 
 
 #python -u main.py --model=RFS     --epochs=20  | tee  model/training_RFS.log
+# No softmax on output (Do'h)
 # Epoch   0 (end) :   Test set: Relation accuracy: 35% | Non-relation accuracy: 17%
 # Epoch  10 (end) :   Test set: Relation accuracy:  0% | Non-relation accuracy: 34%
 
